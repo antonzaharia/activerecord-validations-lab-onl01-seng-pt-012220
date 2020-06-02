4a53ccd0810a13da17d18939c3bf13426d2f7a7a
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-02 17:04:46 +0000

    Automatically backed up by Learn

--- app/validators/non_clickbait_validator.rb
+++ app/validators/non_clickbait_validator.rb
@@ -1,4 +1,4 @@
-class NonClickbaitValidator < ActiveModel::Validator
+class TitleValidator < ActiveModel::Validator
   def validate(record)
     unless record.email.match?(/Won't Believe/, /Secret/, /Top [number]/, /Guess/)
       record.errors[:title] << "We're only allowed non-clickbait titles!"
