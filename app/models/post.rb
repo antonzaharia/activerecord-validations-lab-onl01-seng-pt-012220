d815705a0a1074503769698683497edf3bd49ef6
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-02 17:20:27 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -5,4 +5,13 @@ class Post < ActiveRecord::Base
   validates :category, inclusion: { in: %w(Fiction Non-Fiction),
     message: "%{value} is not a valid category" }
 
+  def is_clickbait
+      included = false
+      phrases = [/won't believe/, /secret/, /top \d/, /guess/]
+      phrases.each { |phrase| included = true if self.title && self.title.downcase =~ phrase }
+      if not included
+          errors.add(:title, "You need a more exciting title")
+      end
+  end
+
 end
