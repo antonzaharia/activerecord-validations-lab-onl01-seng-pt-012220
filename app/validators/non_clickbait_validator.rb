d3eb1df474c6b561b3abc6798e6c26d094d2a2be
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-06-02 17:07:24 +0000

    Automatically backed up by Learn

--- app/validators/non_clickbait_validator.rb
+++
@@ -1,7 +0,0 @@
-class TitleValidator < ActiveModel::Validator
-  def validate(record)
-    unless record.title.starts_with?(/Won't Believe/, /Secret/, /Top [number]/, /Guess/)
-      record.errors[:title] << "We're only allowed non-clickbait titles!"
-    end
-  end
-end
